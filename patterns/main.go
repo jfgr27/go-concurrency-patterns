1e02269e13f56374cd85c8d61946b3c95e70e7d0
James Rea <jamesrea97@gmail.com>
2024-02-14 00:39:50 +0000

    Add MutexWork and pass CLI flags per Command.

--- patterns/main.go
+++ patterns/main.go
@@ -15,25 +15,44 @@ type ConcurrentWork interface {
 func main() {
 	app := &cli.App{
 		Name: "Concurrency pattern benchmark.",
-		Flags: []cli.Flag{
-			&cli.IntFlag{
-				Name:  "workers",
-				Value: 4,
-				Usage: "Number of workers.",
-			},
-			&cli.IntFlag{
-				Name:  "tasks",
-				Value: 1000,
-				Usage: "Number of tasks.",
-			},
-		},
 		Commands: []*cli.Command{
 			{
 				Name:  "pool",
 				Usage: "Pool pattern.",
+				Flags: []cli.Flag{
+					&cli.IntFlag{
+						Name:  "workers",
+						Value: 4,
+						Usage: "Number of workers.",
+					},
+					&cli.IntFlag{
+						Name:  "tasks",
+						Value: 1000,
+						Usage: "Number of tasks.",
+					},
+				},
 				Action: func(cCtx *cli.Context) error {
 					c := PoolWork{cCtx.Int("workers"), cCtx.Int("tasks")}
-					s := fmt.Sprintf("Running pool benchmark with %d workers and %d tasks.", c.w, c.t)
+					s := fmt.Sprintf("Running pool pattern with %d workers and %d tasks.", c.w, c.t)
+					fmt.Println(s)
+
+					c.work()
+					return nil
+				},
+			},
+			{
+				Name:  "mutex",
+				Usage: "Mutex pattern.",
+				Flags: []cli.Flag{
+					&cli.BoolFlag{
+						Name:  "mutex",
+						Value: false,
+						Usage: "With/out mutex.",
+					},
+				},
+				Action: func(cCtx *cli.Context) error {
+					c := MutexWork{cCtx.Bool("mutex")}
+					s := fmt.Sprintf("Running mutex pattern with mutex set to %t.", c.m)
 					fmt.Println(s)
 
 					c.work()
---
+++ patterns/mutex.go
@@ -0,0 +1,96 @@
+package main
+
+
+import (
+	"fmt"
+	"sync"
+)
+type MutexWork struct {
+	m bool
+}
+
+type Container struct {
+    mu       sync.Mutex
+    counters map[string]int
+}
+
+
+
+func (c *Container) incMutex(name string) {
+    c.mu.Lock()
+    defer c.mu.Unlock()
+    c.counters[name]++
+}
+
+func (c *Container) incNoMutex(name string) {
+    c.counters[name]++
+}
+
+func (m *MutexWork) work() {
+
+	if m.m == true {
+		m.mutexWork()
+	} else {
+		m.noMutexWork()
+	}
+}
+func (m *MutexWork) mutexWork() {
+	fmt.Sprintf("Mutex work")
+
+	c := Container{
+		counters: map[string]int{"a": 0, "b": 0},
+	}
+
+	var wg sync.WaitGroup
+
+
+	doIncrement := func(name string, n int) {
+        for i := 0; i < n; i++ {
+            c.incMutex(name)
+        }
+        wg.Done()
+	}
+
+	wg.Add(3)
+
+
+	go doIncrement("a", 10000)
+	go doIncrement("a", 10000)
+	go doIncrement("b", 10000)
+
+	wg.Wait()
+	fmt.Println(c.counters)
+
+
+}
+
+
+func (m *MutexWork) noMutexWork() {
+	fmt.Sprintf("No Mutex work")
+
+	c := Container{
+		counters: map[string]int{"a": 0, "b": 0},
+	}
+
+
+	var wg sync.WaitGroup
+
+	doIncrement := func(name string, n int) {
+		for i := 0; i < n; i++ {
+			c.incNoMutex(name)
+		}
+		wg.Done()
+	}
+
+
+	wg.Add(3)
+
+	go doIncrement("a", 10000)
+	go doIncrement("a", 10000)
+	go doIncrement("b", 10000)
+
+	fmt.Println(c.counters)
+
+
+
+}
